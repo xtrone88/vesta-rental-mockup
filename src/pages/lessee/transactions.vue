afc1143ccc6f4725f460f1a6408113e614695a9f
zess11 <zess11lin@gmail.com>
2021-04-07 16:40:19 -0700

    update transaction page data

--- src/pages/lessee/transactions.vue
+++ src/pages/lessee/transactions.vue
@@ -32,31 +32,14 @@
 
           <v-data-table
             :headers="headers"
-            :items="desserts"
+            :items="transactions"
             item-key="name"
             class="elevation-1"
             :search="search"
             :custom-filter="filterOnlyCapsText"
           >
             <template v-slot:top>
-              <v-text-field
-                v-model="search"
-                label="Search (UPPER CASE ONLY)"
-                class="mx-4"
-              />
-            </template>
-            <template v-slot:[`body.append`]>
-              <tr>
-                <td />
-                <td>
-                  <v-text-field
-                    v-model="calories"
-                    type="number"
-                    label="Less than"
-                  />
-                </td>
-                <td colspan="4" />
-              </tr>
+              <v-text-field v-model="search" label="Search" class="mx-4" />
             </template>
           </v-data-table>
         </v-col>
@@ -68,6 +51,7 @@
 
 <script>
 import moment from "moment";
+import { sampleTransactions } from "../../data/transactions";
 
 export default {
   name: "TransactionsPage",
@@ -76,89 +60,7 @@ export default {
   data: () => ({
     dates: ["2019-09-10", "2019-09-20"],
     search: "",
-    calories: "",
-    desserts: [
-      {
-        name: "Frozen Yogurt",
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: "1%",
-      },
-      {
-        name: "Ice cream sandwich",
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: "1%",
-      },
-      {
-        name: "Eclair",
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: "7%",
-      },
-      {
-        name: "Cupcake",
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: "8%",
-      },
-      {
-        name: "Gingerbread",
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: "16%",
-      },
-      {
-        name: "Jelly bean",
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: "0%",
-      },
-      {
-        name: "Lollipop",
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: "2%",
-      },
-      {
-        name: "Honeycomb",
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: "45%",
-      },
-      {
-        name: "Donut",
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: "22%",
-      },
-      {
-        name: "KitKat",
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: "6%",
-      },
-    ],
+    transactions: sampleTransactions,
   }),
 
   computed: {
@@ -169,25 +71,12 @@ export default {
     },
     headers() {
       return [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        {
-          text: "Calories",
-          value: "calories",
-          filter: (value) => {
-            if (!this.calories) return true;
-
-            return value < parseInt(this.calories);
-          },
-        },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
+        { text: "ID", value: "id" },
+        { text: "Date", value: "createdAt" },
+        { text: "Property Type", value: "propertyType" },
+        { text: "Property Description", value: "propertyDescription" },
+        { text: "Amount", value: "amount" },
+        { text: "Status", value: "status" },
       ];
     },
   },

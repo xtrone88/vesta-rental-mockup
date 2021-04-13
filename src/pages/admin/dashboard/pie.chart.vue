ad27e1378918bc2671d52825d3259fb7b589cc17
zess11 <zess11lin@gmail.com>
2021-04-13 13:24:36 -0700

    fix bug in sidebar with scrolling

--- src/pages/admin/dashboard/pie.chart.vue
+++ src/pages/admin/dashboard/pie.chart.vue
@@ -20,7 +20,7 @@ export default {
     this.gradient2.addColorStop(1, "rgba(0, 231, 255, 0)");
     this.renderChart(
       {
-        labels: ["Books", "Magazines", "Newspapers"],
+        labels: ["Florida", "Louisiana", "Mississippi"],
         datasets: [
           {
             backgroundColor: [this.gradient, this.gradient2, "#00D8FF"],

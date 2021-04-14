9aa52a4a7bd39ba8fbe52c020b5de56b235a1dfb
zess11 <zess11lin@gmail.com>
2021-04-14 13:35:56 -0700

    juniel

--- src/pages/lessee/payments.vue
+++ src/pages/lessee/payments.vue
@@ -42,62 +42,51 @@
           </v-row>
           <v-row>
             <v-col cols="12" sm="2" offset-sm="5">
-              <template>
-                <div>
-                  <v-row
-                    justify="center"
-                  >
-                    <v-btn block class="mt-6 cyan white--text" @click="dialog = true"> ADD PAYMENT </v-btn>
-
-                    <v-dialog
-                      v-model="dialog"
-                      transition="dialog-bottom-transition"
-                      max-width="700"
-                    >
-                      <v-card outlined>
-                        <v-container>
-                          <v-row class="ma-4">
-                            <v-toolbar
-                              flat
-                            >
-                              <v-row align="center" class="font-weight-bold">
-                                <span>Add Payment</span>
-                                <v-spacer></v-spacer>
-                                <v-btn
-                                  icon
-                                  @click="dialog = false"
-                                >
-                                  <v-icon>mdi-close</v-icon>
-                                </v-btn>
-                              </v-row>                            
-                            </v-toolbar>
-                          </v-row>
-                          <v-row class="ma-4">
-                            <v-text-field
-                              label="Payment Information"
-                              prepend-inner-icon="mdi-credit-card"
-                              hint="Please enter card details"
-                              suffix = "MM/YY   CVC"
-                            ></v-text-field>
-                          </v-row>
-                          <v-row class="ma-4 mt-2">
-                            <v-col cols="12" md="3">
-                              <v-btn block class="mt-4 cyan white--text" @click="dialog = false"
-                              > SUBMIT </v-btn>
-                            </v-col>
-                          </v-row>
-                        </v-container>
-                        <div style="flex: 1 1 auto;"></div>
-                      </v-card>
-                    </v-dialog>
-                  </v-row>
-                </div>
-              </template>
+              <v-btn block class="mt-6 cyan white--text" @click="dialog = true"> ADD PAYMENT </v-btn>
             </v-col>
           </v-row>
         </v-card>
       </v-col>
     </v-row>    
+    <v-dialog
+      v-model="dialog"
+      transition="dialog-bottom-transition"
+      max-width="700"
+    >
+      <v-card outlined>
+        <v-row class="ma-4">
+          <v-toolbar
+            flat
+          >
+            <v-row align="center" class="font-weight-bold">
+              <span>Add Payment</span>
+              <v-spacer></v-spacer>
+              <v-btn
+                icon
+                @click="dialog = false"
+              >
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </v-row>                            
+          </v-toolbar>
+        </v-row>
+        <v-row class="ma-4">
+          <v-text-field
+            label="Payment Information"
+            prepend-inner-icon="mdi-credit-card"
+            hint="Please enter card details"
+            suffix = "MM/YY   CVC"
+          ></v-text-field>
+        </v-row>
+        <v-row class="ma-4 mt-2">
+          <v-col cols="12" md="3">
+            <v-btn block class="mt-4 cyan white--text" @click="dialog = false"
+            > SUBMIT </v-btn>
+          </v-col>
+        </v-row>
+        <div style="flex: 1 1 auto;"></div>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 

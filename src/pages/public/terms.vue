7e94b7f9c71f7a0f78258135bfff1cc31900fd63
zess11 <zess11lin@gmail.com>
2021-04-28 08:10:04 -0400

    added date,privacy,terms

--- src/pages/public/terms.vue
+++ src/pages/public/terms.vue
@@ -1,20 +1,69 @@
 <template>
-  <div>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+  <v-container>
+    <div class="terms-of-service">
+      <h1>Terms of Service</h1>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Terms</h1>
-      </v-col>
-    </v-row>
-  </div>
+      <section id="terms-none-americans">
+        <h3>Terms of Service for Non-American Users</h3>
+        <h4>
+          Section 23 of these Terms contains an arbitration agreement and class action waiver that apply to all claims brought against Vesta Leese in the United States. Please read them carefully.
+        </h4>
+        <p>
+          Last Updated: October 30, 2020
+        </p>
+        <p>
+          Thank you for using Vesta Leese!
+        </p>
+        <p>
+          These Terms of Service (“Terms”) are a binding legal agreement between you and Leese that govern your use of the websites, applications, and other offerings from Vesta Leese (collectively, the “Vesta Leese Platform”). When used in these Terms, “Vesta Leese,” “we,” “us,” or “our” refers to the Vesta Leese entity set out on Schedule 1 with whom you are contracting.
+        </p>
+        <p>
+          The Leese Platform offers an online venue that enables users (“Members”) to publish, offer, search for, and book services. Members who publish and offer services are “Hosts” and Members who search for, book, or use services are “Guests.” Hosts offer accommodations (“Accommodations”), activities, excursions, and events (“Experiences”), and a variety of travel and other services (collectively, “Host Services,” and each Host Service offering, a “Listing”). You must register an account to access and use many features of the Vesta Leese Platform, and must keep your account information accurate. As the provider of the Vesta Leese Platform, Vesta Leese does not own, control, offer or manage any Listings or Host Services. Vesta Leese is not a party to the contracts concluded directly between Hosts and Guests, nor is Vesta Leese a real estate broker or insurer. Vesta Leese is not acting as an agent in any capacity for any Member, except as specified in the Payments Terms of Service (“Payment Terms”). To learn more about Vesta Leese role see Section 16.
+        </p>
+        <p>
+          We maintain other terms and policies that supplement these Terms like our Privacy Policy, which describes our collection and use of personal data, and our Payments Terms, which govern any payment services provided to Members by the Vesta Leese payment entities ("Vesta Leese Payments").
+        </p>
+        <p>
+          If you Host, you are responsible for understanding and complying with all laws, rules, regulations and contracts with third parties that apply to your Host Services.
+        </p>
+      </section>
+
+      <section id="table-of-contents">
+        <h3>Table of Contents</h3>
+        <h4>Guest Terms</h4>
+        <p>
+          <a href="#1">1. Our Mission</a>
+        </p>
+        <p>
+          <a href="#2">2. Searching and Booking on Vesta Leese</a>
+        </p>
+        <p>
+          <a href="#3">3. Cancellations, Travel Issues, Refunds and Booking Modifications.</a>
+        </p>
+        <p>
+          <a href="#4">4. Your Responsibilities and Assumption of Risk.</a>
+        </p>
+        <h4>Host Terms</h4>
+        <p>
+          <a href="#5">5. Hosting on Vesta Leese.</a>
+        </p>
+        <p>
+          <a href="#6">6. Managing Your Listing.</a>
+        </p>
+        <p>
+          <a href="#7">7. Cancellations, Travel Issues, and Booking Modifications.</a>
+        </p>
+        <p>
+          <a href="#8">8. Taxes.</a>
+        </p>
+        <h3>Guest Terms</h3>
+        <h4 id="1">1. Our Mission</h4>
+        <p>
+          Our mission is to create a world where you can belong anywhere. From cabins to castles to cooking classes, browse through millions of Listings to find the ones that fit the way you like to travel. Learn more about a Listing by reviewing the description and photos, the Host profile, and Guest reviews. If you have questions, just message the Host.
+        </p>
+      </section>
+    </div>
+  </v-container>
 </template>
 
 <script>

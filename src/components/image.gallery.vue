7fabc80b90c0ac5a0c8aeed9f8ba227378948f21
zess11 <zess11lin@gmail.com>
2021-05-11 06:06:49 -0400

    updated #43

--- src/components/image.gallery.vue
+++ src/components/image.gallery.vue
@@ -154,11 +154,13 @@ export default {
   created() {
     for (let i = 0; i < 5 && i < this.pictures.length; i++) {
       this.gallery.push(
-        i == 0 && this.pictures[i].large ? this.pictures[i].large : this.pictures[i].original
+        this.pictures[i].large ? this.pictures[i].large : this.pictures[i].original
       );
     }
     for (let i = 0; i < this.pictures.length; i++) {
-      this.slider.push(this.pictures[i].large);
+      this.slider.push(
+        this.pictures[i].large ? this.pictures[i].large : this.pictures[i].original
+      );
     }
   },
 };

a40af0310aa15c7aac0e4f3c562fb9c4d0462ee6
zess11 <zess11lin@gmail.com>
2021-05-17 14:08:33 -0700

    add logo and fonts (outside src)

--- src/pages/public/about.vue
+++ src/pages/public/about.vue
@@ -1,7 +1,14 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12"> </v-col>
+      <v-col cols="12">
+        <v-img
+          :src="require('../../assets/vesta_logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">About Page</h1>

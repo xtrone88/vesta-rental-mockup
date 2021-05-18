8f84a3544a36f39089df5549d93d7f44fa15eacd
zess11 <zess11lin@gmail.com>
2021-05-18 14:08:48 -0400

    updated favorite,watching

--- src/pages/lessee/favorites.vue
+++ src/pages/lessee/favorites.vue
@@ -13,134 +13,146 @@
     </v-row>
 
     <v-row v-for="property in properties" :key="property._id">
-        <v-col cols="12" lg="3" md="3">
-          <router-link :to="{ path: '/properties/' + property._id }">
-            <v-img
-              :src="property.pictures[0].large ? property.pictures[0].large : property.pictures[0].original"
-              :lazy-src="property.pictures[0].thumbnail ? property.pictures[0].thumbnail : property.pictures[0].original"
-              class="fill-height rounded-xl"
-              :aspect-ratio="3 / 2"
-            >
-            </v-img>
-          </router-link>
-        </v-col>
-        <v-col class="pl-sm-4" cols="7" sm="6" md="3">
-          <div class="d-flex fill-height flex-column">
-            <div>
-              <div
-                class="font-weight-bold text-subtitle-1"
-              >
+      <v-col cols="12">
+        <v-lazy
+          :options="{
+            threshold: 0.5
+          }"
+          min-height="160">
+          <v-fab-transition mode="out-in">
+            <v-row>
+              <v-col cols="12" lg="3" md="3">
                 <router-link :to="{ path: '/properties/' + property._id }">
-                  {{ property.title }}
+                  <v-img
+                    :src="property.pictures[0].large ? property.pictures[0].large : property.pictures[0].original"
+                    :lazy-src="property.pictures[0].thumbnail ? property.pictures[0].thumbnail : property.pictures[0].original"
+                    class="fill-height rounded-xl"
+                    :aspect-ratio="3 / 2"
+                  >
+                  </v-img>
                 </router-link>
-              </div>
-              <div
-                class="font-weight-regular text-sm-body-2 text-caption"
-              >
-                {{ property.address.street }}<br />
-                {{ property.address.city + "," + property.address.country }}
-              </div>
-            </div>
-            <div class="mt-auto">
-              <div
-                class="font-weight-regular text-sm-body-2 text-caption mb-2"
-              >
-                {{ property.capacity }}
-              </div>
-              <div>
-                <v-btn
-                  small
-                  elevation="0"
-                  color="pink"
-                  class="px-6 white--text"
-                  :to="{ path: `/properties/${property._id}` }"
-                >
-                  <v-icon class="rotate-270" left>mdi-hammer</v-icon>
-                  BID NOW
-                </v-btn>
-              </div>
-            </div>
-          </div>
-        </v-col>
-        <v-col class="d-none d-sm-flex justify-center" cols="3" sm="3">
-          <div class="d-flex fill-height flex-column align-baseline">
-            <div class="d-inline-block text-center">
-              <div
-                class="font-weight-bold text-lg-h6 text-md-subtitle-1 text-sm-body-2 text-caption"
-              >
-                Current Bid
-              </div>
-              <v-chip
-                class="d-inlie-block justify-center"
-                style="width: 100px"
-                color="cyan"
-                outlined
-                label
-              >
-                $1,000
-              </v-chip>
-            </div>
-            <div class="mt-auto mb-4 d-inline-block text-center">
-              <div
-                class="font-weight-bold text-lg-h6 text-md-subtitle-1 text-sm-body-2 text-caption"
-              >
-                Total Bids
-              </div>
-              <v-chip
-                class="d-inlie-block justify-center"
-                style="width: 100px"
-                color="cyan"
-                outlined
-                label
-              >
-                20
-              </v-chip>
-            </div>
-          </div>
-        </v-col>
-        <v-col cols="5" sm="2">
-          <div class="d-flex fill-height flex-column justify-space-between">
-            <div class="text-sm-body-2 text-caption">
-              <div>TAKING POST BIDS</div>
-              <div class="font-weight-bold">
-                {{ property.postbids }} days
-              </div>
-              <div>Ends {{ property.postBidsEndingDate }}</div>
-            </div>
-            <div
-              class="mt-2 text-sm-body-2 text-caption"
-            >
-              <div>Lease Start</div>
-              <div class="font-weight-bold">
-                {{ property.leaseStartDate }}
-              </div>
-              <div>Lease End</div>
-              <div class="font-weight-bold">
-                {{ property.leaseEndDate }}
-              </div>
-            </div>
-          </div>
-        </v-col>
-        <v-col class="d-flex d-sm-none mt-2" cols="12">
-          <v-row no-gutters>
-            <v-col cols="6" class="border-top">
-              <div class="text-subtitle-2 text-center">
-                <span class="cyan--text">Current Bid</span><br />
-                $1,000
-              </div>
-            </v-col>
-            <v-divider :vertical="true"></v-divider>
-            <v-col cols="6" class="border-top">
-              <div class="text-subtitle-2 text-center">
-                <span class="cyan--text">Total Bids</span><br />
-                20
-              </div>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="12">
-          <v-divider class="mt-sm-4"></v-divider>
-        </v-col>
+              </v-col>
+              <v-col class="pl-sm-4" cols="7" sm="6" md="3">
+                <div class="d-flex fill-height flex-column">
+                  <div>
+                    <div
+                      class="font-weight-bold text-subtitle-1"
+                    >
+                      <router-link :to="{ path: '/properties/' + property._id }">
+                        {{ property.title }}
+                      </router-link>
+                    </div>
+                    <div
+                      class="font-weight-regular text-sm-body-2 text-caption"
+                    >
+                      {{ property.address.street }}<br />
+                      {{ property.address.city + "," + property.address.country }}
+                    </div>
+                  </div>
+                  <div class="mt-auto">
+                    <div
+                      class="font-weight-regular text-sm-body-2 text-caption mb-2"
+                    >
+                      {{ property.capacity }}
+                    </div>
+                    <div>
+                      <v-btn
+                        small
+                        elevation="0"
+                        color="pink"
+                        class="px-6 white--text"
+                        :to="{ path: `/properties/${property._id}` }"
+                      >
+                        <v-icon class="rotate-270" left>mdi-hammer</v-icon>
+                        BID NOW
+                      </v-btn>
+                    </div>
+                  </div>
+                </div>
+              </v-col>
+              <v-col class="d-none d-sm-flex justify-center" cols="3" sm="3">
+                <div class="d-flex fill-height flex-column align-baseline">
+                  <div class="d-inline-block text-center">
+                    <div
+                      class="font-weight-bold text-lg-h6 text-md-subtitle-1 text-sm-body-2 text-caption"
+                    >
+                      Current Bid
+                    </div>
+                    <v-chip
+                      class="d-inlie-block justify-center"
+                      style="width: 100px"
+                      color="cyan"
+                      outlined
+                      label
+                    >
+                      $1,000
+                    </v-chip>
+                  </div>
+                  <div class="mt-auto mb-4 d-inline-block text-center">
+                    <div
+                      class="font-weight-bold text-lg-h6 text-md-subtitle-1 text-sm-body-2 text-caption"
+                    >
+                      Total Bids
+                    </div>
+                    <v-chip
+                      class="d-inlie-block justify-center"
+                      style="width: 100px"
+                      color="cyan"
+                      outlined
+                      label
+                    >
+                      20
+                    </v-chip>
+                  </div>
+                </div>
+              </v-col>
+              <v-col cols="5" sm="2">
+                <div class="d-flex fill-height flex-column justify-space-between">
+                  <div class="text-sm-body-2 text-caption">
+                    <div>TAKING POST BIDS</div>
+                    <div class="font-weight-bold">
+                      {{ property.postbids }} days
+                    </div>
+                    <div>Ends {{ property.postBidsEndingDate }}</div>
+                  </div>
+                  <div
+                    class="mt-2 text-sm-body-2 text-caption"
+                  >
+                    <div>Lease Start</div>
+                    <div class="font-weight-bold">
+                      {{ property.leaseStartDate }}
+                    </div>
+                    <div>Lease End</div>
+                    <div class="font-weight-bold">
+                      {{ property.leaseEndDate }}
+                    </div>
+                  </div>
+                </div>
+              </v-col>
+              <v-col class="d-flex d-sm-none mt-2" cols="12">
+                <v-row no-gutters>
+                  <v-col cols="6" class="border-top">
+                    <div class="text-subtitle-2 text-center">
+                      <span class="cyan--text">Current Bid</span><br />
+                      $1,000
+                    </div>
+                  </v-col>
+                  <v-divider :vertical="true"></v-divider>
+                  <v-col cols="6" class="border-top">
+                    <div class="text-subtitle-2 text-center">
+                      <span class="cyan--text">Total Bids</span><br />
+                      20
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col cols="12">
+                <v-divider class="mt-sm-4"></v-divider>
+              </v-col>
+            </v-row>
+          </v-fab-transition>
+        </v-lazy>
+      </v-col>
     </v-row>
 
     <v-snackbar v-model="watched"
